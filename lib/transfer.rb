660890c6479c70575da96ad89d763a35f4cabfc5
Marcus  = Soulo <souloinc87@gmail.com>
2018-04-26 01:20:43 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -19,7 +19,7 @@ class Transfer
      @sender.balance -= @amount
      @receiver.balance += @amount
      @status = "complete"
-     binding.pry 
+     binding.pry
   else
      "Transaction rejected. Please check your account balance. "
      @status = "rejected"
