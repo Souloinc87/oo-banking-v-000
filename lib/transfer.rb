d24bfeee5c4c7e50ec199079767b8ebd024b553f
Marcus  = Soulo <souloinc87@gmail.com>
2018-04-26 01:21:35 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,11 +15,11 @@ class Transfer
   end
 
   def execute_transaction
-  if @sender.valid? == true 
+  if @sender.valid?  
      @sender.balance -= @amount
      @receiver.balance += @amount
      @status = "complete"
-     binding.pry
+     #binding.pry
   else
      "Transaction rejected. Please check your account balance. "
      @status = "rejected"
