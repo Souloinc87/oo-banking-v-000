0ce3ef065bcdb8ba4ff1040bd8bc05e5e9f2de6d
Marcus  = Soulo <souloinc87@gmail.com>
2018-04-26 01:40:56 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   end
 
   def execute_transaction
-  if valid? && self.status == "pending"
+  if valid? && self.status == "pending" && @sender.balance >= 50
      @sender.balance -= @amount
      @receiver.balance += @amount
      self.status = "complete"
